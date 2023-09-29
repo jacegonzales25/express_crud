35a891432364f55d2ea244055746bc05c57c5136
Jace L. Gonzales <jacegonzales25@gmail.com>
2023-09-29 21:55:33 +0800

    Running application

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./**/*.{html,js}",
-    "./views/**/*.{html,ejs,js}"
+    "./views/**/*.{html,js,ejs}",
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require("daisyui")
+  ],
 }
 
